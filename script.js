5adf8d25bcce4d1e9be868ea982a597db6cc834f
JB Orozco <35300800+jborozco@users.noreply.github.com>
2023-08-04 13:10:33 +0200

    Prevent the first empty book to be loaded

--- script.js
+++ script.js
@@ -12,47 +12,51 @@ let myLibrary = [
     }
 ];
 
+const initialLibrary = myLibrary;
+
 function Book() {
-    let titleValue = document.getElementById('book-title').value;
-    let authorValue = document.getElementById('book-author').value;
-    let dateValue = document.getElementById('book-date').value;
+    titleValue = document.getElementById('book-title').value;
+    authorValue = document.getElementById('book-author').value;
+    dateValue = document.getElementById('book-date').value;
+
 
     this.title = titleValue;
     this.author = authorValue;
     this.bookDate = dateValue;
 }
 
-
-
-function addBookToLibrary() {
-    //Add a book to myLibrary
-
-    newBook = new Book();
-    myLibrary.push(newBook);
-
-
-
+function displayBooks() {
     //Update DOM Library with all books
     library.innerHTML = ""; //reset library
     for (let i = 0; i < myLibrary.length; i++) { //update library with current library
         library.innerHTML += `
-        <div class="library-book">
-            <p class="library-book-title">${myLibrary[i].title}</p>
-            <p class="library-book-author">${myLibrary[i].author}</p>
-            <p class="library-book-date">${myLibrary[i].bookDate}</p>
-        </div>
-        `
-
+            <div class="library-book">
+                <p class="library-book-title">${myLibrary[i].title}</p>
+                <p class="library-book-author">${myLibrary[i].author}</p>
+                <p class="library-book-date">${myLibrary[i].bookDate}</p>
+            </div>
+            `
     }
-    //clear fields
+    //Clear form fields
     titleValue = "";
     authorValue = "";
     dateValue = "";
+}
+
+
+function addBookToLibrary() {
+
+    //Add a book to myLibrary
+    newBook = new Book();
+    myLibrary.push(newBook);
+    //Update DOM
+    displayBooks();
 
 };
 
 
 
+//Initiate DOM Library on first load
+displayBooks();
 
-addBookToLibrary();
 
